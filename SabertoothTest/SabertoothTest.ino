024b87705351951036f25a6462362eb889a052e6
Lukas Severinghaus <lukasseveringhaus@gmail.com>
2016-08-25 20:07:12 -0400

    Update SabertoothTest

--- SabertoothTest/SabertoothTest.ino
+++ SabertoothTest/SabertoothTest.ino
@@ -13,30 +13,31 @@ then stopping for 2.5 seconds.
 
 
 
-#include <Servo.h> 
-
+#include <Servo.h>// Import the Servo library 
+/* Initialize two constant ints to hold the
+pin numbers for the two channels*/
 const int turnServoPin = 5;
 const int linearServoPin = 6;
  
-Servo turnServo;
+Servo turnServo;//Create servo objects for the two channels
 Servo linearServo;
 
-void setup() 
+void setup() // This function run once at start
 { 
-  turnServo.attach(turnServoPin);
+  turnServo.attach(turnServoPin);// associate the servo object with the pin
   linearServo.attach(linearServoPin);
-  turnServo.write(90);
+  turnServo.write(90);// set the angle of the servo to 90 degrees
   linearServo.write(90);
   
 } 
  
  
-void loop() 
+void loop() //Runs continuously after start
 { 
-  linearServo.write(100);
+  linearServo.write(100);//Set the servos to new positions
   turnServo.write(100);
-  delay(2500);
-  linearServo.write(90);
+  delay(2500);// Wait for 2.5 seconds
+  linearServo.write(90);//Stop both servos
   turnServo.write(90);
-  delay(2500);
+  delay(2500);// Wait for 2.5 seconds
 } 
